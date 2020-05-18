f2bfca79f006af7a82321c6533bfba4a6d314339
jau1jz <95550641@163.com>
2020-05-18 18:00:21 +0800

    go version

--- main.go
+++ main.go
@@ -17,16 +17,7 @@ func main() {
 
 	log.Printf("双色球一等奖号码计算系统。\n")
 
-	log.Printf("你需要计算几组双色球号码:\n")
-
 	input := bufio.NewScanner(os.Stdin)
-loop:
-	input.Scan()
-	NumCount, err := strconv.Atoi(input.Text())
-	if err != nil {
-		log.Println("错误,请输入数字类型!!")
-		goto loop
-	}
 	log.Printf("需要计算多少次（百万）:\n")
 loop2:
 	input.Scan()
@@ -35,15 +26,19 @@ loop2:
 		log.Println("错误,请输入数字类型!!")
 		goto loop2
 	}
+	CalcCount = CalcCount * 1e6
 	wait := sync.WaitGroup{}
-	wait.Add(NumCount)
-	for i := 0; i < NumCount; i++ {
-		log.Printf("开始计算第%d组。", i+1)
+
+	begin := time.Now()
+	numMap := map[string]int{}
+	rand.Seed(time.Now().UnixNano())
+	waitCalc := sync.WaitGroup{}
+	waitCalc.Add(4)
+	splitCount := CalcCount / 4
+	locker := sync.Mutex{}
+	for step := 0; step < 4; step++ {
 		go func() {
-			begin := time.Now()
-			numMap := map[string]int{}
-			rand.Seed(time.Now().UnixNano())
-			for i := 0; i < CalcCount*1e6; i++ {
+			for k := 0; k < splitCount; k++ {
 				nums := []int{}
 				find := func(num int) bool {
 					for _, v := range nums {
@@ -67,20 +62,25 @@ loop2:
 				blueNum := rand.Intn(16)
 				nums = append(nums, blueNum+1)
 				sort.Ints(nums[:6])
+				locker.Lock()
 				numMap[fmt.Sprintf("%v", nums)]++
+				locker.Unlock()
 			}
-			max := ""
-			times := 0
-			for k, v := range numMap {
-				if v > times {
-					times = v
-					max = k
-				}
-			}
-			pass := time.Now().Sub(begin)
-			log.Printf("pass %ds key:%s , times :%d", pass/1e9, max, times)
-			wait.Done()
+			waitCalc.Done()
 		}()
 	}
+
+	waitCalc.Wait()
+	max := ""
+	times := 0
+	for k, v := range numMap {
+		if v > times {
+			times = v
+			max = k
+		}
+	}
+	pass := time.Now().Sub(begin)
+	log.Printf("pass %ds key:%s , times :%d", pass/1e9, max, times)
 	wait.Wait()
+
 }
